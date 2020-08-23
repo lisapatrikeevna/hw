1d619a116bd282d95ef92f335b8b44c23bc34688
lisapatrikeevna <38157157+lisapatrikeevna@users.noreply.github.com>
2020-08-24 01:30:53 +0300

    Updates

---
+++ precache-manifest.c4612422b26e4d251c96049673c95377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "208426deb12cb081eb7b4f17202784d2",
+    "url": "/lisapatrikeevna/hw.git/index.html"
+  },
+  {
+    "revision": "da099bc38b33ed575cd0",
+    "url": "/lisapatrikeevna/hw.git/static/css/main.c59f52ee.chunk.css"
+  },
+  {
+    "revision": "29e6236d824109f26310",
+    "url": "/lisapatrikeevna/hw.git/static/js/2.c8e9ee60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lisapatrikeevna/hw.git/static/js/2.c8e9ee60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da099bc38b33ed575cd0",
+    "url": "/lisapatrikeevna/hw.git/static/js/main.e8c8adfd.chunk.js"
+  },
+  {
+    "revision": "ca1ae808e777c39a0daa",
+    "url": "/lisapatrikeevna/hw.git/static/js/runtime-main.5eb24e7b.js"
+  }
+]);
